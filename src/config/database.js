353ffee3af824d8bcdeb7e809240206e6dbe03c7
CarlosCala <carlosdev52@gmail.com>
2023-04-29 14:26:49 -0300

    corrections

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,5 @@
 import mongoose, { connection } from "mongoose";
 
-// const url = "mongodb://127.0.0.1:27017/rollingPizza";
 
 const url = 'mongodb+srv://bellaPizza:a4yaii7fIu6kzM4z@cluster0.swomave.mongodb.net/rollingPizza'
 
